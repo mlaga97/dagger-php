87b7e060da4f8916d6eb12cb2cd97734c52a702a
Luc Lagarde <matieu.lagarde@usm.edu>
2016-10-05 10:28:46 -0500

    Only do bitmask if pediatric assessment selected

--- include/insert.php
+++ include/insert.php
@@ -675,25 +675,27 @@ score_questions($_SESSION);
 
 	// Do the other side of pediatric.php
 	// TODO: Much of this could be moved into the bitmask library.
-	$pediatric_bitmask_fields = array("HLS_FH_Diab", "HLS_FH_HBP", "HLS_FH_HD", "HLS_FH_Overwt");
-	foreach($pediatric_bitmask_fields as $field_name) {
-		$sum = 0;
+	if($_SESSION['pediatric_check']) {
+		$pediatric_bitmask_fields = array("HLS_FH_Diab", "HLS_FH_HBP", "HLS_FH_HD", "HLS_FH_Overwt");
+		foreach($pediatric_bitmask_fields as $field_name) {
+			$sum = 0;
 
-		$sum += $_SESSION[$field_name . '-1'];
-		$sum += $_SESSION[$field_name . '-2'];
-		$sum += $_SESSION[$field_name . '-3'];
-		$sum += $_SESSION[$field_name . '-4'];
-		$sum += $_SESSION[$field_name . '-5'];
-		$sum += $_SESSION[$field_name . '-6'];
-		
-		unset($_SESSION[$field_name . '-1']);
-		unset($_SESSION[$field_name . '-2']);
-		unset($_SESSION[$field_name . '-3']);
-		unset($_SESSION[$field_name . '-4']);
-		unset($_SESSION[$field_name . '-5']);
-		unset($_SESSION[$field_name . '-6']);
-		
-		$_SESSION[$field_name] = $sum;
+			$sum += $_SESSION[$field_name . '-1'];
+			$sum += $_SESSION[$field_name . '-2'];
+			$sum += $_SESSION[$field_name . '-3'];
+			$sum += $_SESSION[$field_name . '-4'];
+			$sum += $_SESSION[$field_name . '-5'];
+			$sum += $_SESSION[$field_name . '-6'];
+
+			unset($_SESSION[$field_name . '-1']);
+			unset($_SESSION[$field_name . '-2']);
+			unset($_SESSION[$field_name . '-3']);
+			unset($_SESSION[$field_name . '-4']);
+			unset($_SESSION[$field_name . '-5']);
+			unset($_SESSION[$field_name . '-6']);
+
+			$_SESSION[$field_name] = $sum;
+		}
 	}
 
 
