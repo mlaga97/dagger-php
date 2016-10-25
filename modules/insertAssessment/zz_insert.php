7b9bb9c07f896d278853a9ee1b72d00dc1b5be65
Luc Lagarde <matieu.lagarde@usm.edu>
2016-10-25 16:30:47 -0500

    Clean up insertion statements

--- modules/insertAssessment/zz_insert.php
+++ modules/insertAssessment/zz_insert.php
@@ -1,39 +1,74 @@
 <?php
+
+	// These keys should not be inserted into the DB
+	$insert_blacklist = array(
+			'admin', 'university_id', 'grouping', 'logo', 'status', 'previous', 'test_acc',
+			'n1', 'n2', 'n3', 'n4',
+			'st', 'id', 'GRHOP_standard', 'c_p_id', 'search_select', 'contact_date',
+			'entry_date', 'contact_outcome', 'outcome_other', 'contact_reason', 'reason_other',
+	);
+
+	// These keys should be encrypted to protect patient privacy 
+	$insert_encrypted_whitelist = array('pt_id', 'first_name', 'last_name');
+
+	// These keys need to have empty values substituted with 0000-00-00 in order to please the DB
+	$insert_dates_whitelist = array('A1CDate', 'eAGDate', 'cholestoralDate', 'bpDate', 'physicalDate', 'hospital_visit_date', 'er_visit_date', 'office_visit_date', 'assessment_date');
+
+	/**************************************************************************
+	***************************************************************************
+	**************************************************************************/
+
 	$keys = "(id,";
 	$values = "(0, ";
-	foreach($_SESSION as $key=>$value) { 	//We want to insert the values contained in $_SESSION not $copy.
-		if (($key != 'st')  && ($key != 'status') && ($key != 'id') &&($key != 'logo') && ($key != 'n1') && ($key != 'n2')  //took out st, so if it barfs, we'll leave it in.
-				&& ($key != 'n3')  && ($key != 'n4') && ($key != 'GRHOP_standard') && ($key != 'admin') && ($key != 'c_p_id')       //throw st away. I can't figure out where this is comming from in adult.php.
-				&& ($key != 'previous') && ($key != 'search_select') && ($key != 'university_id')&& ($key != 'grouping')
-				&& ($key != 'contact_date') && ($key != 'entry_date') && ($key != 'contact_outcome')
-				&& ($key != 'outcome_other') && ($key != 'contact_reason') && ($key != 'reason_other') && ($key != 'test_acc')) {
-					$keys = $keys .   $key . " ,";
-					if($key == 'pt_id' || $key == 'first_name' || $key == 'last_name') { // Here we are setting the name and pt_id to password.
-						$value = strtolower($value);
-						$value = hash('sha256', $value);
-						$values = $values . " '" . $value . "',";
-					} else {
-						if ((($key === 'A1CDate')|| ($key === 'eAGDate') ||($key === 'cholestoralDate') ||
-								($key === 'bpDate')||($key === 'physicalDate') || ($key === 'hospital_visit_date')||
-								($key === 'er_visit_date') || ($key === 'office_visit_date')||($key === 'assessment_date')) && ($value === '')) {
-									$values = $values ." '" . "0000-00-00" . "',";
-								} else {
-									$values = $values . " '" . $value . "',";
-								}
-					}
-				}
+
+	// Pull the desired keys from $_SESSION and do some final post-processing
+	foreach($_SESSION as $key=>$value) {
+
+		// Ignore keys in blacklist
+		// TODO: Perhaps a whitelist would be less likely to fail upon insertion?
+		if( !array_key_exists($key, $insert_blacklist) ) {
+
+			// Append key to list of keys
+			$keys = $keys . $key . " ,";
+
+			if( array_key_exists($key, $insert_encrypted_whitelist) ) {
+
+				// Encrypt values to protect patient privacy
+				$value = strtolower($value);
+				$value = hash('sha256', $value);
+				$values = $values . " '" . $value . "',";
+
+			} elseif( array_key_exists($key, $insert_dates_whitelist) && ($value === '') ) {
+
+				// Empty date values need to be substituted with 0000-00-00 in order to please the DB
+				$values = $values ." '" . "0000-00-00" . "',";
+			} else {
+
+				// All other values can just be appended as-is
+				$values = $values . " '" . $value . "',";
+			}
+		}
 	}
+
+	// Generate query for insertion into DB
 	$keys = $keys . "date)";
 	$values = $values . "NOW())";
 	$query = "insert into response $keys values $values";
-	if ($_SESSION['grouping'] !=10) { //grouping = 10 means the user is logged in as a CFHC employee. We are not keeping their data only scoring assessments.
+
+	// Insert query into DB unless the user is not a CFHC employee, as we only score their assessments.
+	if ($_SESSION['grouping'] != 10) {
+
+		// Insert
 		$result = $mysqli->query($query);
+
+		// Keep track of insert_id just in case we need to edit.
+		// TODO: Is this necessary with reviewAssessment.php?
 		$insert_id = $mysqli->insert_id;
 		$_SESSION['insert_id'] = $insert_id;
-		if ($_SESSION['grouping'] ==10) { //see note above.
-			$log->info("INSERT LOG: " . $today ." response.id: CFHC non-insert ". $_SESSION['user_id'] ." ". $query);
-		} else {
-			$log->info("INSERT LOG: " . $today ." response.id: " .$insert_id. " ". $_SESSION['user_id'] ." ". $query);
-		}
+
+		// Log, just in case.
+		$log->info("INSERT LOG: " . $today ." response.id: " .$insert_id. " ". $_SESSION['user_id'] ." ". $query);
+	} else {
+		$log->info("INSERT LOG: " . $today ." response.id: CFHC non-insert ". $_SESSION['user_id'] ." ". $query);
 	}
 ?>
