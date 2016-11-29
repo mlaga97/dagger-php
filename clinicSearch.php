7c114f12534128a648bfee56aca2cf4705e3fa77
Luc Lagarde <matieu.lagarde@usm.edu>
2016-11-29 09:19:09 -0600

    Finish up clinicSearch features

--- clinicSearch.php
+++ clinicSearch.php
@@ -4,6 +4,12 @@
 	allowPrevious($_SESSION['admin'] == 1, '/clinicSearch.php');
 
 	$mysqli = dbOpen();
+
+	// TODO: Move elsewhere
+	function validateDate($date) {
+		$d = DateTime::createFromFormat('Y-m-d', $date);
+		return $d && $d->format('Y-m-d') === $date;
+	}
 ?>
 
 <!-- HTML start -->
@@ -38,18 +44,18 @@
 
 			<div style='text-align: center;'>
 				<?php
+					// We can go ahead and get the user and clinic list with
+					// no real processing logic.
 					$userIds = $mysqli->query('SELECT id,name from users where id in (select user_id from groups where clinic_id in(select clinic_id from groups where user_id = ' . $_SESSION['user_id'] . '));');
 					$clinicIds = $mysqli->query('SELECT id,name from clinic where id in (select clinic_id from groups where user_id = ' . $_SESSION['user_id'] . ');');
 
+					// We start with the most general request, which is to find
+					// the number of patients for the clinic group associated
+					// with the user that is logged in.
 					$results_query = 'SELECT COUNT(id) AS clients FROM response WHERE clinic_id IN (select clinic_id from groups where user_id = ' . $_SESSION['user_id'] . ') ';
 
-					// TODO: Move elsewhere
-					function validateDate($date)
-					{
-						$d = DateTime::createFromFormat('Y-m-d', $date);
-						return $d && $d->format('Y-m-d') === $date;
-					}
-
+					// Then, for each possible form value, we narrow the search
+					// down according to those criteria (if applicable).
 					if(array_key_exists('clinicStats_startDate', $_POST) && validateDate($_POST['clinicStats_startDate'])) {
 						$results_query = $results_query . ' AND date > "' . $_POST['clinicStats_startDate']. '"';
 					}
@@ -63,27 +69,39 @@
 						$results_query = $results_query;
 					}
 
-					echo $results_query;
-
-					// If statements for various combinations of inputs
+					// Once we have narrowed down as far as possible, we make
+					// the query to the database, fetch the first row, then
+					// check the value of 'clients', as this will be our result
 					$results = $mysqli->query($results_query);
+					$result_array = mysqli_fetch_assoc($results);
+					$result = $result_array['clients'];
 				?>
 
 				<form action='clinicSearch.php' method="post">
-					<select name='clinicStats_clinicId'>
+
+					<!-- Take the clinic list and generate a selection box with them, using the last value if possible. -->
+					Clinic: <select name='clinicStats_clinicId'>
+						<option <?php if(!array_key_exists('clinicStats_clinicId', $_POST)) echo 'selected'; ?>></option>
 						<?php while( ($clinicId = mysqli_fetch_assoc($clinicIds)) ): ?>
 							<option value='<?php echo $clinicId['id']; ?>' <?php if($_POST['clinicStats_clinicId'] == $clinicId['id']) echo 'selected'; ?>><?php echo $clinicId['name']; ?></option>
 						<?php endwhile; ?>
 					</select>
 
-					<select name='clinicStats_userId'>
+					<br/><br/>
+
+					<!-- Take the user list and generate a selection box with them, using the last value if possible. -->
+					Employee: <select name='clinicStats_userId'>
+						<option <?php if(!array_key_exists('clinicStats_userId', $_POST)) echo 'selected'; ?>></option>
 						<?php while( ($userId = mysqli_fetch_assoc($userIds)) ): ?>
 							<option value='<?php echo $userId['id']; ?>' <?php if($_POST['clinicStats_userId'] == $userId['id']) echo 'selected'; ?>><?php echo $userId['name']; ?></option>
 						<?php endwhile; ?>
 					</select>
 
-					Start date: <input type='date' name='clinicStats_startDate' value='<?php $_POST['clinicStats_startDate']?>'>
-					End date: <input type='date' name='clinicStats_endDate' value='<?php $_POST['clinicStats_endDate']?>'>
+					<br/><br/>
+
+					<!-- Switched to HTML5 date input type, in order to standardize and reduce dependencies. Try to use last value, if possible. -->
+					Start date: <input type='date' name='clinicStats_startDate' value='<?php echo $_POST['clinicStats_startDate']?>'>
+					End date: <input type='date' name='clinicStats_endDate' value='<?php echo $_POST['clinicStats_endDate']?>'>
 
 					<br><br>
 					<input type='submit'>
@@ -91,7 +109,7 @@
 
 				<br/><br/>
 
-				<h2>These parameters yield <?php // Print results here ?> patients so far!</h2>
+				<h2>These parameters yield <?php echo $result ?> patients so far!</h2>
 			</div>
 
 			<br/><br/><br/>
