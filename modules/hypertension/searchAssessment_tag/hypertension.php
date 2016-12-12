97f08e361edb3352e9e27707d62537ac81c3b2e1
Luc Lagarde <matieu.lagarde@usm.edu>
2016-12-12 13:53:16 -0600

    Make viewAssessment not fail better
    For some reason, a wildcard does not work in this situation. This is probably worth looking into, as it is extremely difficult to realize just by looking at it.

--- modules/hypertension/searchAssessment_tag/hypertension.php
+++
@@ -1,6 +0,0 @@
-<?php
-	global $row;
-
-	if ($row ['hypertension_check'] == 1)
-		echo "Hypertension ";
-?>
--- modules/pediatric/searchAssessment_tag/pediatric.php
+++
@@ -1,6 +0,0 @@
-<?php
-	global $row;
-
-	if ($row ['pediatric_check'] == 1)
-		echo "Pediatric Lifestyles ";
-?>
