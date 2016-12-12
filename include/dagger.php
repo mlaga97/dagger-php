ed79b472bf3736d32adc940b6f8e8e2d6363e097
Luc Lagarde <matieu.lagarde@usm.edu>
2016-12-12 16:41:26 -0600

    Enable multiple JSON configuration files

--- include/dagger.php
+++ include/dagger.php
@@ -151,11 +151,25 @@
 			}
 		}
 	}
-	
-	function getConfigKey($key) {
-		$configString = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/config.json');
-		$config = json_decode($configString, true);
 
+	function getConfig() {
+		$path = $_SERVER['DOCUMENT_ROOT'] . '/config.json';
+		$contents = file_get_contents($path);
+		$config = json_decode($contents, true);
+
+		foreach(moduleList() as $module) {
+			if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/modules/' . $module . '/config.json')) {
+				$path = $_SERVER['DOCUMENT_ROOT'] . '/modules/' . $module . '/config.json';
+				$contents = file_get_contents($path);
+				$config = array_merge_recursive($config, json_decode($contents, true));
+			}
+		}
+
+		return $config;
+	}
+
+	function getConfigKey($key) {
+		$config = getConfig();
 		return $config[$key];
 	}
 
