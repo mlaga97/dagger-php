a7a97aeb1995b57d82b54a03f2439aee7811dea9
Luc Lagarde <matieu.lagarde@usm.edu>
2016-12-14 11:18:13 -0600

    Make password change better whilst removing other settings

--- userSettings.php
+++ userSettings.php
@@ -2,21 +2,6 @@
 	include 'include/dagger.php';
 	global $log, $mysqli, $today;
 
-	// Update User Record
-	$update_query = 'UPDATE users ';
-	foreach($_POST as $key=>$value) {
-		if($key == "name" || $key == "uname" || $key == "pswd") {
-			$update_query = 'UPDATE users SET ' . $key . '="' . $value . '" WHERE id=' . $_SESSION['user_id'] . ';';
-		} else {
-			$update_query = 'UPDATE users SET ' . $key . '=' . $value . ' WHERE id=' . $_SESSION['user_id'] . ';';
-		}
-		$null = $mysqli->query($update_query);
-	}
-
-	// Force logout to reload settings.
-	// TODO: Not this.
-	allowPrevious($_SERVER['REQUEST_METHOD'] !== 'POST', '/userSettings.php');
-
 	// Get Current User Record
 	$query = 'SELECT id, name, uname, pswd, university_id, clinic_id, admin, employee_id, grouping, test_acc FROM users WHERE id = ' . $_SESSION['user_id'] . ' LIMIT 1';
 	$info = $mysqli->query($query);
@@ -54,19 +39,30 @@
 
 			<div style='text-align: center;'>
 		        <form action="userSettings.php" method="post">
-					id:<br><input type="text" name="id" value="<?php echo $row['id'] ?>"><br><br>
-					name:<br><input type="text" name="name" value="<?php echo $row['name'] ?>"><br><br>
-					uname:<br><input type="text" name="uname" value="<?php echo $row['uname'] ?>"><br><br>
-					pswd:<br><input type="text" name="pswd" value="<?php echo $row['pswd'] ?>"><br><br>
-					university_id:<br><input type="text" name="university_id" value="<?php echo $row['university_id'] ?>"><br><br>
-					clinic_id:<br><input type="text" name="clinic_id" value="<?php echo $row['clinic_id'] ?>"><br><br>
-					admin:<br><input type="text" name="admin" value="<?php echo $row['admin'] ?>"><br><br>
-					employee_id:<br><input type="text" name="employee_id" value="<?php echo $row['employee_id'] ?>"><br><br>
-					grouping:<br><input type="text" name="grouping" value="<?php echo $row['grouping'] ?>"><br><br>
-					test_acc:<br><input type="text" name="test_acc" value="<?php echo $row['test_acc'] ?>"><br><br>
+					Current Password:<br/><input type="password" name="oldPassword"><br><br>
+					New Password:<br/><input type="password" name="newPassword"><br><br>
+					Confirm Password:<br/><input type="password" name="confirmPassword" onkeyup="checkPass(); return false;"><br><br>
 
 					<input type="submit" value="Submit">
 				</form>
+
+				<?php
+					// Update User Record
+					if(array_key_exists('oldPassword', $_POST) && array_key_exists('newPassword', $_POST) && array_key_exists('confirmPassword', $_POST)) {
+						if($row['pswd'] == $_POST['oldPassword']) {
+							if($_POST['newPassword'] == $_POST['confirmPassword']) {
+								$update_query = 'UPDATE users SET pswd = "' . $_POST['newPassword'] . '" WHERE id=' . $_SESSION['user_id'] . ';';
+								$null = $mysqli->query($update_query);
+
+								echo 'Password updated successfully!';
+							} else {
+								echo 'Failed to update password: Passwords do not match!';
+							}
+						} else {
+							echo 'Failed to update password: Current Password incorrect!';
+						}
+					}
+				?>
 			</div>
 
 	        <br/><br/><br/>
