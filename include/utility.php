389c40403c9b0ba8951ea09ea0acfc95e61bc962
Luc Lagarde <matieu.lagarde@usm.edu>
2017-01-09 10:48:33 -0600

    Add documentation to utility.php

--- include/utility.php
+++ include/utility.php
@@ -1,30 +1,35 @@
 <?php /* Function Library */
 
+	/**
+	 * Unsets the keys $_SESSION from a blacklist.
+	 * 
+	 * @param string[] $keysToUnset Blacklist of keys to unset.
+	 */
 	function unsetKeys($keysToUnset) {
-
 		foreach($keys as $key) {
 			unset($_SESSION[$key]);
 		}
-
 	}
 
+	/**
+	 * Unsets all keys in $_SESSION except those from a whitelist.
+	 *
+	 * @param string[] $keysToUnset Whitelist of keys to keep.
+	 */
 	function unsetAllButTheseKeys($keysToKeep) {
-
-		// Go through each key in $_SESSION
 		foreach ($_SESSION as $key) {
-
-			// Check if key is safe
 			if(!array_key_exists($key, $keysToKeep)) {
-
-				// Unset if not
 				unset($_SESSION[$key]);
-
 			}
-
 		}
-
 	}
 
+	/**
+	 * Copies entire $_POST array to $_SESSION, with the ability to ignore
+	 * certain keys using a blacklist.
+	 * 
+	 * @param string[] $ignoreKeys List of keys to ignore.
+	 */
 	function postToSession($ignoreKeys = array()) {
 		foreach($_POST as $key=>$value) {
 			if (!array_key_exists($key, $ignoreKeys)) {
@@ -33,6 +38,14 @@
 		}
 	}
 
+	/**
+	 * Check if a string contains a match among any of the provided patterns.
+	 * 
+	 * @param string[] $patterns Array of regular expressions to attempt to
+	 * match.
+	 * @param string $subject The string to attempt to match.
+	 * @return boolean True if a match was found.
+	 */
 	function multiPregMatch($patterns, $subject) {
 		foreach($patterns as $pattern) {
 			if(preg_match($pattern, $subject)) {
