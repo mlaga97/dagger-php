3729bb3dd65605d58387ba2d703fade5faf92627
Michael Graham <michael.d.graham@usm.edu>
2017-04-05 17:22:49 -0500

    Fix reviewAssessment demographics

--- modules/demographics/reviewAssessment/1_demographics.php
+++ modules/demographics/reviewAssessment/1_demographics.php
@@ -2,38 +2,16 @@
 	////////////////This is where we will print our strings for our results page////////////////////////////
 	if ($_SESSION['grouping']== 10){ echo '<div id="demo_table" <?php style="display: none;">';} else {echo ' <div id="demo_table">';}
 	echo "<table border=\"1\" width=\"800\">"; //Printing a magical 14!?
-	echo "<th bgcolor = \"D8D8D8\" width = \"800\" colspan=\"2\"><font size = \"6\"><center>Demographic Information</center></font></th>";
-	if ($_SESSION['first_name']!= '') {
-		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">";
-		echo "<b>First Name: </b>";
-		echo "</td><td width = \"400\">";
-		Print_r($_SESSION['first_name']);
-	echo "</td></tr>";
-	} else {
-		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">";
-		echo "<b>First Name:</b>";
-		echo "</td><td width = \"400\">";
-		echo " Data unspecified.";
-		echo "</td></tr>";
-	}
-	if ($_SESSION['last_name']!= '') {
-		echo "<tr bgcolor=\"#D8D8D8\"><td width = \"200\">";
-		echo "<b>Last Name: </b>";
-		echo "</td><td width = \"400\">";
-		Print_r($_SESSION['last_name']);
-	echo "</td></tr>";
-	} else {
-		echo "<tr bgcolor=\"#D8D8D8\"><td width = \"200\">";
-		echo "<b>Last Name:</b>";
-		echo "</td><td width = \"400\">";
-		echo " Data unspecified.";
-		echo "</td></tr>";
-	}
-	if ($_SESSION['pt_id']!= '') {
+	echo "<th bgcolor = \"D8D8D8\" width = \"800\" colspan=\"2\"><center><h3>Demographic Information</h3></center></th>";
+
+	// Removed first and last name
+
+	// Updated Session field names
+	if ($_SESSION['patientID']!= '') {
 		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">";
 		echo "<b>Patient ID: </b>";
 		echo "</td><td width = \"400\">";
-		Print_r($_SESSION['pt_id']);
+		Print_r($_SESSION['patientID']);
 	echo "</td></tr>";
 	} else {
 		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">";
@@ -55,11 +33,11 @@
 		echo " Data unspecified.";
 		echo "</td></tr>";
 	}
-	if ($_SESSION['zip']!= '') {
-		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">"; 
+	if ($_SESSION['demographics_zipCode']!= '') {
+		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">";
 		echo "<b>Zip:</b> ";
 		echo "</td><td width = \"400\">";
-		Print_r($_SESSION['zip']);
+		Print_r($_SESSION['demographics_zipCode']);
 		echo "</td></tr>";
 	} else {
 		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">";
@@ -68,24 +46,24 @@
 		echo " Data unspecified.";
 		echo "</td></tr>";
 	}
-	if ($_SESSION['sex']!= '') {
+	if ($_SESSION['demographics_gender']!= '') {
 		echo "<tr bgcolor=\"#D8D8D8\"><td width = \"200\">";
-		echo "<b>Sex:</b> ";
+		echo "<b>Gender:</b> ";
 		echo "</td><td width = \"400\">";
-		Print_r($_SESSION['sex']);
+		Print_r($_SESSION['demographics_gender']);
 		echo "</td></tr>";
 	} else {
 		echo "<tr bgcolor=\"#D8D8D8\"><td width = \"200\">";
-		echo "<b>Sex:</b>";
+		echo "<b>Gender:</b>";
 		echo "</td><td width = \"400\">";
 		echo " Data unspecified.";
 		echo "</td></tr>";;
 	}
-	if ($_SESSION['eth']!= '') {
-		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">"; 
+	if ($_SESSION['demographics_ethnicity']!= '') {
+		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">";
 		echo "<b>Ethnicity:</b> ";
 		echo "</td><td width = \"400\">";
-		Print_r($_SESSION['eth']);
+		Print_r($_SESSION['demographics_ethnicity']);
 		echo "</td></tr>";
 	} else {
 		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">";
@@ -94,11 +72,11 @@
 		echo " Data unspecified.";
 		echo "</td></tr>";
 	}
-	if ($_SESSION['m_status']!= '') {
+	if ($_SESSION['demographics_maritalStatus']!= '') {
 		echo "<tr bgcolor=\"#D8D8D8\"><td width = \"200\">";
 		echo "<b>Marital Status:</b> ";
 		echo "</td><td width = \"400\">";
-		Print_r($_SESSION['m_status']);
+		Print_r($_SESSION['demographics_maritalStatus']);
 		echo "</td></tr>";
 	} else {
 		echo "<tr bgcolor=\"#D8D8D8\"><td width = \"200\">";
@@ -107,11 +85,11 @@
 		echo " Data unspecified.";
 		echo "</td></tr>";
 	}
-	if ($_SESSION['ed']!= '') {
-		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">"; 
+	if ($_SESSION['demographics_education']!= '') {
+		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">";
 		echo "<b>Education:</b> ";
 		echo "</td><td width = \"400\">";
-		Print_r($_SESSION['ed']);
+		Print_r($_SESSION['demographics_education']);
 		echo "</td></tr>";
 	} else {
 		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">";
@@ -120,11 +98,11 @@
 		echo " Data unspecified.";
 		echo "</td></tr>";
 	}
-	if ($_SESSION['living']!= '') {
+	if ($_SESSION['demographics_livingArrangements']!= '') {
 		echo "<tr bgcolor=\"#D8D8D8\"><td width = \"200\">";
 		echo "<b>Living</b> ";
 		echo "</td><td width = \"400\">";
-		Print_r($_SESSION['living']);
+		Print_r($_SESSION['demographics_livingArrangements']);
 		echo "</td></tr>";
 	} else {
 		echo "<tr bgcolor=\"#D8D8D8\"><td width = \"200\">";
@@ -133,20 +111,8 @@
 		echo " Data unspecified.";
 		echo "</td></tr>";
 	}
-	if ($_SESSION['assessment_type'] == 'Child') {
-		if ($_SESSION['c_bo']!="") {
-		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">"; 
-		echo "<b>Birth Order:</b> ";
-		echo "</td><td width = \"400\">";
-		Print_r($_SESSION['c_bo']);
-		echo "</td></tr>";
-	} else {
-		echo "<tr bgcolor=\"#FAFAFA\"><td width = \"200\">"; 
-		echo "<b>Birth Order:</b> ";
-		echo "</td><td width = \"400\">";
-		echo " Data unspecified.";
-		echo "</td></tr>";
-		}
-	}
+
+// Removed child birth order
+
 	echo "</table><br></div>";
 ?>
