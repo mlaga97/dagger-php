148305cea514260665d0684e228adb8ab492f53b
Luc Lagarde <matieu.lagarde@usm.edu>
2017-05-16 22:16:09 -0500

    Slightly less bad implementation of jsonAssessment

--- modules/jsonAssessment/assessment/jsonAssessment.php
+++ modules/jsonAssessment/assessment/jsonAssessment.php
@@ -1,8 +1,27 @@
 <?php
+function areFriends($types, $question1, $question2) {
+	if($question1["type"] == $question2["type"]) {
+		return true;
+	} else {
+		if(array_key_exists("friends", $types[$question1["type"]])) {
+			if(in_array($question2["type"], $types[$question1["type"]]["friends"])) {
+				return true;
+			}
+		}
+		if(array_key_exists("friends", $types[$question2["type"]])) {
+			if(in_array($question1["type"], $types[$question2["type"]]["friends"])) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
 function renderQuestionSection($questions, $types) {
 	foreach($questions as $questionNumber=>$question) {
 		// NOTE: This takes advantage of Short-Circuit evaluation and will break otherwise
-		if($questionNumber == 0 || $question["type"] != $questions[$questionNumber-1]["type"]) {
+		if($questionNumber == 0 || !areFriends($types, $question, $questions[$questionNumber-1])) {
 
 			$typeData = $types[$question["type"]];
 			switch($typeData["type"]) {
@@ -17,21 +36,26 @@ function renderQuestionSection($questions, $types) {
 
 		}
 
-		echo "<tr><td>" . $question["text"] . "</td>";
-
 		$typeData = $types[$question["type"]];
 		switch($typeData["type"]) {
 			case "radioScale":
+				if($questionNumber == 0) {
+					echo "<br/>";
+				}
+				echo "<tr><td>" . $question["text"] . "</td>";
 				foreach($typeData["options"] as $optionText => $value) {
 					echo "<td><center><input type='checkbox' name='" . $question["id"] . "' value='" . $value . "' /></center></td>";
 				}
 				break;
 			case "radioOptions":
-				echo "<br/>";
+				if($questionNumber != 0) {
+					echo "<hr/>";
+				}
+				echo "<br/>" . $question["text"] . "<br/><br/>";
 				foreach($typeData["options"] as $optionText => $value) {
 					echo "<label><input type='checkbox' name='" . $question["id"] . "' value='" . $value . "' />" . $optionText . "</label><br/>";
 				}
-				echo "<hr/>";
+				echo "<br/>";
 				break;
 		}
 		echo "</tr>";
@@ -51,16 +75,17 @@ foreach($assessments as $assessment) {
 
 	if(array_key_exists("questions", $assessment)) {
 		renderQuestionSection($assessment["questions"], $assessment["types"]);
+		echo "<hr/>";
 	}
 
 	if(array_key_exists("sections", $assessment)) {
 		foreach($assessment["sections"] as $section) {
 			if(array_key_exists("description", $section))
-				echo $section["description"] . "<br/>";
+				echo "<br/>" . $section["description"] . "<br/>";
 			renderQuestionSection($section["questions"], $assessment["types"]);
+			echo "<hr/>";
 		}
 	}
-
-	echo "<hr/>";
+	
 }
 ?>
