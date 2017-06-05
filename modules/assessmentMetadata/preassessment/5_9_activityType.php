525959b33964e5bffd04f119f991cbf40f6558d5
Luc Lagarde <matieu.lagarde@usm.edu>
2017-06-05 18:43:19 -0500

    Add activityType "Historical"

--- modules/assessmentMetadata/preassessment/5_9_activityType.php
+++ modules/assessmentMetadata/preassessment/5_9_activityType.php
@@ -5,6 +5,7 @@
 <label><input type="radio" name="activityType" value="Warm Hand Off and Consult"/>Warm Hand Off and Consult</label><br/>
 <label><input type="radio" name="activityType" value="Consult Only"/>Consult Only</label><br/>
 <label><input type="radio" name="activityType" value="Phone Call"/>Phone Call</label><br/>
+<label><input type="radio" name="activityType" value="Historical"/>Historical</label><br/>
 <label><input type="radio" name="activityType" value="No Show"/>No Show</label><br/>
 
 <br/><br/>
