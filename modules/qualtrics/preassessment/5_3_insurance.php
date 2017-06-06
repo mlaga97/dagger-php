dc140202672429c1b632394b47b575deb3a40ccb
Luc Lagarde <matieu.lagarde@usm.edu>
2017-06-05 20:37:19 -0500

    Add preassessment insurance question and keys

---
+++ modules/qualtrics/preassessment/5_3_insurance.php
@@ -0,0 +1,9 @@
+<h3>Insurance</h3>
+
+<label><input type="radio" name="insurance" value="1" required />Yes</label><br/>
+<label><input type="radio" name="insurance" value="0" required />No</label><br/>
+<label><input type="radio" name="insurance" value="-1" checked />No Response</label><br/>
+
+<br/><br/>
+
+<hr/>
