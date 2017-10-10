1ef07380e99da5da10960ab89cf85d12829611fa
Luc Lagarde <matieu.lagarde@usm.edu>
2017-10-10 15:21:30 -0500

    Move database initialization from include/dagger.php to include/db.php

--- include/dagger.php
+++ include/dagger.php
@@ -20,17 +20,10 @@
 		}
 	}
 
-	// Import the libraries needed for MySQL to be loaded
-	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/module.php');
-	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/config.php');
+	// Set up database
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/db.php');
 
-	// MySQL Setup
-	$mysqli = new mysqli(
-		getConfigKey("edu.usm.dagger.main.db.server"),
-		getConfigKey("edu.usm.dagger.main.db.user"),
-		getConfigKey("edu.usm.dagger.main.db.password")
-	);
-
+	// Ensure that everything is set up properly
 	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/setupCheck.php');
 
 	// Import the dependencies for user access
