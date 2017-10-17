0cce8c939b29c486d9f46a2617c18a899198ab9b
Luc Lagarde <matieu.lagarde@usm.edu>
2017-10-17 07:59:04 -0500

    Assorted whitespace fixes

--- include/response.php
+++ include/response.php
@@ -51,4 +51,5 @@
 
 		return $output;
 	}
+
 ?>
--- include/uri.php
+++ include/uri.php
@@ -1,15 +1,17 @@
 <?php
 
-function getURIPath($uri) {
-	$splitURI = explode('?', $uri);
-	return $splitURI[0];
-}
+	function getURIPath($uri) {
+		$splitURI = explode('?', $uri);
+		return $splitURI[0];
+	}
 
-function explodeURI($uri) {
-	$path = explode('/', getURIPath($uri));
+	function explodeURI($uri) {
+		$path = explode('/', getURIPath($uri));
 
-	// Get rid of blank first element
-	array_shift($path);
+		// Get rid of blank first element
+		array_shift($path);
 
-	return $path;
-}
+		return $path;
+	}
+
+?>
