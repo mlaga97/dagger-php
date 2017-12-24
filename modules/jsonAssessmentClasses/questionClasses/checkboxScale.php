9fd21ed95d0b50e3456d8768702801cb58c247bb
Luc Lagarde <matieu.lagarde@usm.edu>
2017-12-23 19:10:53 -0600

    Add checkboxScale

---
+++ modules/jsonAssessmentClasses/questionClasses/checkboxScale.php
@@ -0,0 +1,28 @@
+<?php
+
+global $questionClasses;
+$questionClasses["checkboxScale"] = array();
+
+$questionClasses["checkboxScale"]["header"] = function($options) {
+	echo "<table><tr><th>Question</th>";
+	echo "<th></th>";
+	echo "</tr>";
+};
+
+$questionClasses["checkboxScale"]["render"] = function($question, $relativeQuestionNumber, $absoluteQuestionNumber, $type) {
+	if($relativeQuestionNumber == 0) {
+		echo "<br/>";
+	}
+	echo "<tr><td><ol start='" . $absoluteQuestionNumber .  "'><li>" . $question["text"] . "</li></ol></td>";
+	echo "<td><center><label class='checkbox_caption'><br/><input type='checkbox' name='" . $question["id"] . "' value='1' /><br/></label></center></td>";
+};
+
+$questionClasses["checkboxScale"]["parseResponse"] = function($rawAnswer, $type) {
+	if($rawAnswer == $type["emptyValue"]) {
+		return "No Response";
+	}
+
+	return array_search($rawAnswer, $type["options"]);
+}
+
+?>
