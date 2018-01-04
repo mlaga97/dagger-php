335a16fe236583352a7ce662405c775b49b63142
Luc Lagarde <matieu.lagarde@usm.edu>
2018-01-04 11:48:11 -0600

    Move stressor inputs to jsonAssessment

--- modules/old_assessments/assessment/assessments.php
+++ modules/old_assessments/assessment/assessments.php
@@ -2,11 +2,7 @@
 	echo "<!--oldassessments-assessments-assessments.php -->";
 	global $mysqli;
 
-	include __DIR__ . '/../../stressors/stressors.php';
-	include __DIR__ . '/../../current_stress/current_stress.php';
-	include __DIR__ . '/../../adhd/adhd.php';
 	include __DIR__ . '/../../presenting_problem/presenting_problem.php';
-	include __DIR__ . '/../../childStressors/childStressors.php';
 
 	if($_SESSION['pp_check'] == 1) {
 		echo "<div class='write'>";
@@ -14,31 +10,4 @@
 		write_presenting_problem($_SESSION['assessment_type'], $mysqli);
 		echo "</div>";
 	}
-
-	if ($_SESSION['c_stress_check'] == 1) {
-		echo "<div class='write'>";
-		// Assessment header in fxn
-		write_current_stress("Child");
-		echo "</div>";
-	}
-
-	if ($_SESSION['c_stress_check'] == 1) {
-		echo "<div class='write'>";
-		// Assessment header in fxn
-		write_childStressors($_SESSION['assessment_type'], $mysqli);
-		echo "</div>";
-	}
-
-
-	if($_SESSION['stress_check'] == 1) {
-		echo "<div class='write'>";
-		// Assessment header in fxn
-		write_stressors($_SESSION['assessment_type'], $mysqli);
-		echo "</div>";
-		echo "<div class='write'>";
-		// Assessment header in fxn
-		write_current_stress($_SESSION['assessment_type']);
-		echo "</div>";
-	}
-
 ?>
--- modules/old_assessments/preassessment/aa_oldassessments.php
+++ modules/old_assessments/preassessment/aa_oldassessments.php
@@ -1,6 +1,4 @@
 <?php
-	$_SESSION['stress_check'] = 0;
-	$_SESSION['c_stress_check'] = 0;
 	$_SESSION['pp_check'] = 0;
 
 	// Current Grouping is as follows:
@@ -16,12 +14,6 @@
 
 	if ($_SESSION['grouping'] == 1) { //groupong = 1 is for test accounts.
 		echo "
-			<div title=\"This section contains questions pertaining to the client's current stress level and specific stressors.\">
-				<label><input id=\"stress_check\" input type=\"checkbox\" name=\"stress_check\" value=\"1\" />Stress</label>
-			</div>
-			<div title=\"This section contains questions pertaining to the client's current stress level and specific stressors.\">
-				<label><input id=\"c_stress_check\" input type=\"checkbox\" name=\"c_stress_check\" value=\"1\" />Child Stressors</label>
-			</div>
 			<div title=\"This section contains questions pertaining to the client's presenting problem.\">
 				<label><input id=\"pp_check\" input type=\"checkbox\" name=\"pp_check\" value=\"1\" />Presenting Problem</label>
 			</div>
@@ -31,22 +23,6 @@
 					<div class=\"childonly\" title=\"This section contains questions pertaining to the client's presenting problem.\">
 						<label><input id=\"pp_check\" input type=\"checkbox\" name=\"pp_check\" value=\"1\" />Presenting Problem</label>
 					</div>
-
-					<div title=\"This section contains questions pertaining to the client's current stress level and specific stressors.\">
-						<label><input id=\"stress_check\" input type=\"checkbox\" name=\"stress_check\" value=\"1\" />Stress</label>
-					</div>
-		";
-	} else if ($_SESSION['grouping'] == 6) { //grouping = 6 is for the MS GRHOP non social work students
-		echo "
-			<div title=\"This section contains questions pertaining to the client's current stress level and specific stressors.\">
-				<label><input id=\"stress_check\" input type=\"checkbox\" name=\"stress_check\" value=\"1\" />Stress</label>
-			</div>
-		";
-	} else { //default is everything
-		echo "
-			<div title=\"This section contains questions pertaining to the client's current stress level and specific stressors.\">
-				<label><input id=\"stress_check\" input type=\"checkbox\" name=\"stress_check\" value=\"1\" />Stress</label>
-			</div>
 		";
 	}
 ?>
