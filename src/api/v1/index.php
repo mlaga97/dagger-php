07e7634a9241c8a7c8349e4ad73d25536db50a85
Matieu Lagarde <matieu.lagarde@usm.edu>
2018-11-08 00:17:59 -0600

    Move setup from include/dagger.php into api/v1/index.php

--- src/api/v1/index.php
+++ src/api/v1/index.php
@@ -1,8 +1,16 @@
 <?php
 
-	// Set noRedirect to allow importing dagger.php without redirecting
-	// TODO: Find a more elegant solution
-	$noRedirect = true;
+	// Log4php Setup
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/log4php/Logger.php');
+	Logger::configure($_SERVER['DOCUMENT_ROOT'] . '/include/log4php/config.xml');
+	$log = Logger::getLogger('myLogger');
+
+	// Set Timezone Data
+	date_default_timezone_set('America/Chicago');
+  $today = date('m-d-y h:i:s');
+
+	// User session
+  session_start();
 
 	// Ensure that the api can be used externally
 	// TODO: Figure out what the proper thing to do here is, as opposed to just adding things until it works.
@@ -18,8 +26,16 @@
 	}
 	header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
 
-	// Load libraries
-	require_once '../../include/dagger.php';
+	// Set up database
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/db.php');
+
+	// Ensure that everything is set up properly
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/include/setupCheck.php');
+
+	// Import the dependencies for user access
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/include/access.php');
+
+  // Load libraries
 	require_once '../../include/json.php';
 	require_once '../../include/AltoRouter/AltoRouter.php';
 
