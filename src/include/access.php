822dc950904fc194275f3fe71d12f397bf2c2e55
Matieu Lagarde <matieu.lagarde@usm.edu>
2018-11-08 00:18:23 -0600

    Remove unused code

--- src/include/access.php
+++ src/include/access.php
@@ -1,67 +1,6 @@
 <?php
 
 	/**
-	 * Defines page access rules and declares new page name.
-	 * 
-	 * Page access rules can be provided in several forms, including:
-	 * - string	   Name of allowed previous page
-	 * - string[]  Names of allowed previous pages
-	 * - bool      Value or expression to test
-	 * 
-	 * If a string or string[] is given, the page will log the user out if the
-	 * name of the previous page is not present in the page access rules. If a
-	 * boolean is given, the page will log the user out if the expression
-	 * returns false.
-	 * 
-	 * If the user is not logged out, then the current page is assigned the new
-	 * name provided.
-	 * 
-	 * NOTE: This check will be skipped if the debug flag for the user is set
-	 * 
-	 * @param string|string[]|boolean $access_whitelist New page access rules to enforce.
-	 * 
-	 * @param string $new_name New name to give to current page.
-	 */
-	function allowPrevious($access_whitelist, $new_name) {
-
-		// Check debug flag first
-		if(array_key_exists("debug", $_SESSION) && $_SESSION["debug"]) {
-			$_SESSION['previous'] = $new_name;
-			return;
-		}
-
-		$type = gettype($access_whitelist);
-
-		switch ($type) {
-			case "string":
-				if($_SESSION['previous'] != $access_whitelist) {
-					header("location: /index.php");
-					die("Access denied, redirecting!");
-				}
-				break;
-
-			case "array":
-				if(!in_array($_SESSION['previous'], $access_whitelist)) {
-					header("location: /index.php");
-					die("Access denied, redirecting!");
-				}
-				break;
-
-			case "boolean":
-				if(!$access_whitelist) {
-					header("location: /index.php");
-					die("Access denied, redirecting!");
-				}
-				break;
-
-			default:
-				die("checkPrevious requires a string, string[], or boolean");
-		}
-
-		$_SESSION['previous'] = $new_name;
-	}
-
-	/**
 	 * Attempts to login with username and password.
 	 * 
 	 * If the login attempt fails, an error message will be returned, otherwise
@@ -93,31 +32,6 @@
 			return true;
 		} else {
 			return "Please enter a correct username and password.";
-		}
-	}
-
-
-	/**
-	 * Reloads the user configuration and logs them out if the user is no longer
-	 * active or does not exist.
-	 */
-	function reloadUserSettings() {
-		global $log, $mysqli;
-
-		$query = 'SELECT ';
-		foreach(getConfigKey("edu.usm.dagger.main.login.user.keys") as $key) {
-			$query .= $key . ', ';
-		}
-		$query .= 'users.id AS user_id FROM users WHERE users.id = "' . $_SESSION['user_id'] . '" AND active = 1 LIMIT 1';
-
-		$results = $mysqli->query($query);
-
-		if($results && $results->num_rows === 1) {
-			foreach($results->fetch_assoc() as $key => $value) {
-				$_SESSION[$key] = $value;
-			}
-		} else {
-			header("location: /index.php");
 		}
 	}
 
