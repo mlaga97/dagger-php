a4c69f7e6ee46c8cc8f6c06dd183cace9e55e4a9
Matieu Lagarde <matieu.lagarde@usm.edu>
2018-11-09 15:14:39 -0600

    Add ability to search certain assessment_responses keys

--- src/include/search.php
+++ src/include/search.php
@@ -6,7 +6,9 @@ function getSearch($parameters) {
   $query = '';
   $search = false;
 
+  // Handle searches on traditional columns
   $LUT = [
+    'id' => 'id',
     'userID' => 'user_id =',
     'clinicID' => 'clinic_id =',
     'patientID' => 'patient_id =',
@@ -16,7 +18,6 @@ function getSearch($parameters) {
     'dateSubmittedEnd' => 'date_submitted <=',
     'dateSubmittedStart' => 'date_submitted >=',
   ];
-
   foreach($LUT as $key => $value) {
     if(array_key_exists($key, $parameters)) {
       if($search) {
@@ -25,6 +26,43 @@ function getSearch($parameters) {
 
       $search = true;
       $query .= $value . ' "' . $mysqli->real_escape_string($parameters[$key]) . '"';
+    }
+  }
+
+  // TODO: Handle searches on selected_assessments
+
+  // Handle searches on assessment_responses
+  // TODO: Derive LUT from config, not hardcoded.
+  $LUT = [
+    // Demographics
+    'gender' => '$.demographics.gender',
+    'ethnicity' => '$.demographics.ethnicity',
+    'maritalStatus' => '$.demographics.maritalStatus',
+    'livingArrangements' => '$.demographics.livingArrangements',
+    'zip' => '$.demographics.zip',
+    'education' => '$.demographics.education',
+
+    // MIHDP
+    'hasInsurance' => '$.mihdp.hasInsurance',
+    'behavioralHealth' => '$.mihdp.behavioralHealth',
+    'chronicCare' => '$.mihdp.chronicCare',
+    'nutritionCounseling' => '$.mihdp.nutritionCounseling',
+    'activityType' => '$.mihdp.activityType',
+
+    // MIHDP Programs
+    'hepC' => '$.programs.hepC',
+    'ryanWhite' => '$.programs.ryanWhite',
+    'homeless' => '$.programs.homeless',
+  ];
+  foreach($LUT as $key => $value) {
+    if(array_key_exists($key, $parameters)) {
+      if($search) {
+        $query .= ' AND ';
+      }
+
+      $search = true;
+      $query .= "(JSON_CONTAINS(assessment_responses, '\"" . $mysqli->real_escape_string($parameters[$key]) . "\"', '$value')";
+      $query .= " OR JSON_CONTAINS(assessment_responses, '" . $mysqli->real_escape_string($parameters[$key]) . "', '$value'))";
     }
   }
 
