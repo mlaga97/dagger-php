904d9af9a91fe727a679269b14886dbf9e9352cd
Matieu Lagarde <matieu.lagarde@usm.edu>
2018-11-09 13:45:09 -0600

    Rewrite getSearch in a much more generic way

--- src/include/search.php
+++ src/include/search.php
@@ -1,74 +1,31 @@
 <?php
 
-// TODO: Rewrite this in a much more generic way
 function getSearch($parameters) {
   global $mysqli;
 
   $query = '';
   $search = false;
 
-  if(array_key_exists('visitDateStart', $parameters)) {
-    if($search)
-      $query .= ' AND ';
+  $LUT = [
+    'userID' => 'user_id =',
+    'clinicID' => 'clinic_id =',
+    'patientID' => 'patient_id =',
+    'patientDOB' => 'patient_dob =',
+    'visitDateEnd' => 'visit_date <=',
+    'visitDateStart' => 'visit_date >=',
+    'dateSubmittedEnd' => 'date_submitted <=',
+    'dateSubmittedStart' => 'date_submitted >=',
+  ];
 
-    $search = true;
-    $query .= 'visit_date >= "' . $mysqli->real_escape_string($parameters['visitDateStart']) . '"';
-  }
+  foreach($LUT as $key => $value) {
+    if(array_key_exists($key, $parameters)) {
+      if($search) {
+        $query .= ' AND ';
+      }
 
-  if(array_key_exists('visitDateEnd', $parameters)) {
-    if($search)
-      $query .= ' AND ';
-
-    $search = true;
-    $query .= 'visit_date <= "' . $mysqli->real_escape_string($parameters['visitDateEnd']) . '"';
-  }
-
-  if(array_key_exists('dateSubmittedStart', $parameters)) {
-    if($search)
-      $query .= ' AND ';
-
-    $search = true;
-    $query .= 'date_submitted >= "' . $mysqli->real_escape_string($parameters['dateSubmittedStart']) . '"';
-  }
-
-  if(array_key_exists('dateSubmittedEnd', $parameters)) {
-    if($search)
-      $query .= ' AND ';
-
-    $search = true;
-    $query .= 'date_submitted <= "' . $mysqli->real_escape_string($parameters['dateSubmittedEnd']) . '"';
-  }
-
-  if(array_key_exists('userID', $parameters)) {
-    if($search)
-      $query .= ' AND ';
-
-    $search = true;
-    $query .= 'user_id = ' . $mysqli->real_escape_string($parameters['userID']);
-  }
-
-  if(array_key_exists('clinicID', $parameters)) {
-    if($search)
-      $query .= ' AND ';
-
-    $search = true;
-    $query .= 'clinic_id = ' . $mysqli->real_escape_string($parameters['clinicID']);
-  }
-
-  if(array_key_exists('patientID', $parameters)) {
-    if($search)
-      $query .= ' AND ';
-
-    $search = true;
-    $query .= 'patient_id = ' . $mysqli->real_escape_string($parameters['patientID']);
-  }
-
-  if(array_key_exists('patientDOB', $parameters)) {
-    if($search)
-      $query .= ' AND ';
-
-    $search = true;
-    $query .= 'patient_dob = ' . $mysqli->real_escape_string($parameters['patientDOB']);
+      $search = true;
+      $query .= $value . ' "' . $mysqli->real_escape_string($parameters[$key]) . '"';
+    }
   }
 
   if($search) {
