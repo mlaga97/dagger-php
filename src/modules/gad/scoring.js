7f929c013666c5dcd2097c7e26d27e78a0446440
Jonathon Evans <w994126@usm.edu>
2018-12-07 13:18:13 -0600

    Add Scoring functionality for GAD7
    TODO: recommendation

---
+++ src/modules/gad/scoring.js
@@ -0,0 +1,42 @@
+window.scoring.gad = {};
+
+window.scoring.gad.score = function(response, flags) {
+  result = {};
+    result.valid = true;
+
+  Object.keys(response).forEach(function(key) {
+    response[key] = parseInt(response[key]);
+    if(!(response[key] >= 0)) {
+        result.valid = false;
+    }
+  });
+
+
+	if(result.valid) {
+		result.score = 0;
+        Object.keys(response).forEach(function(key) {
+            result.score += response[key];
+        }); 
+
+    // TODO: Verify case
+		
+        if(result.score <= 4) {
+            result.severity = 'Minimal anxiety';
+            result.recommendation = 'N/A';
+        } else if(result.score <= 9) {
+            result.severity = 'Mild anxiety';
+            result.recommendation = 'N/A';
+        } else if(result.score <= 14) {
+            result.severity = 'Moderate anxiety';
+            result.recommendation = 'N/A';
+        } else if(result.score <= 21) {
+            result.severity = 'Severe anxiety';
+            result.recommendation = 'N/A';
+        }
+	}
+
+  return {
+    result,
+    flags,
+  };
+}
