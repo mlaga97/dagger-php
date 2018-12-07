fdab086e3aaecb52257ec9314af924d55a0ee442
Jonathon Evans <w994126@usm.edu>
2018-12-07 13:07:25 -0600

    Add Scoring functionality for Dast

--- src/modules/dast/scoring.js
+++ src/modules/dast/scoring.js
@@ -23,13 +23,13 @@ window.scoring.dast.score = function(response, flags) {
         if(result.score == 0) {
             result.severity = 'No problems reported';
             result.recommendation = 'None at this time';
-        } else if(result <= 2) {
+        } else if(result.score <= 2) {
             result.severity = 'Low level';
             result.recommendation = 'Monitor, reassess at a later date';
-        } else if(result <= 5) {
+        } else if(result.score <= 5) {
             result.severity = 'Moderate level';
             result.recommendation = 'Further investigation is required';
-        } else if(result <= 8) {
+        } else if(result.score <= 8) {
             result.severity = 'Substantional level';
             result.recommendation = 'Assessment required';
         } else {
