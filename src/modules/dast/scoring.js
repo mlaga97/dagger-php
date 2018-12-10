18c719fbcc7817c9c83ec942362bd3a325c14e95
Jonathon Evans <31749551+jonathon-evans@users.noreply.github.com>
2018-12-10 15:16:56 -0600

    Dast (#26)
    * Add Scoring for Dast
    
    couldn't test because I don't have the password. Plan to test tomorrow
    
    * Add Scoring functionality for Dast
    
    * Fix Typo and Get Rid Of ADHD

---
+++ src/modules/dast/scoring.js
@@ -0,0 +1,45 @@
+window.scoring.dast = {};
+
+window.scoring.dast.score = function(response, flags) {
+  result = {};
+    result.valid = true;
+
+  Object.keys(response).forEach(function(key) {
+    response[key] = parseInt(response[key]);
+    if(!(response[key] >= 0)) {
+        result.valid = false;
+    }
+  });
+
+
+	if(result.valid) {
+		result.score = 0;
+        Object.keys(response).forEach(function(key) {
+            result.score += response[key];
+        }); 
+
+    // TODO: Verify case
+		
+        if(result.score == 0) {
+            result.severity = 'No problems reported';
+            result.recommendation = 'None at this time';
+        } else if(result.score <= 2) {
+            result.severity = 'Low level';
+            result.recommendation = 'Monitor, reassess at a later date';
+        } else if(result.score <= 5) {
+            result.severity = 'Moderate level';
+            result.recommendation = 'Further investigation is required';
+        } else if(result.score <= 8) {
+            result.severity = 'Substantial level';
+            result.recommendation = 'Assessment required';
+        } else {
+            result.severity = 'Severe level';
+            result.recommendation = 'Assessment required';
+        }
+	}
+
+  return {
+    result,
+    flags,
+  };
+}
