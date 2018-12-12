86665b7bf0c225dcfe11bea37df0ab2d1cb798f5
Jonathon Evans <w994126@usm.edu>
2018-12-12 09:52:06 -0600

    Add Scoring for PCL

---
+++ src/modules/pcl/scoring.js
@@ -0,0 +1,42 @@
+window.scoring.pcl = {};
+
+window.scoring.pcl.score = function(response, flags) {
+  result = {};
+    result.valid = true;
+
+  Object.keys(response).forEach(function(key) {
+    response[key] = parseInt(response[key]);
+    if(!(response[key] >= 0)) {
+        result.valid = false;
+    }
+  });
+
+
+	if(result.valid) {
+		result.score = 0;
+        Object.keys(response).forEach(function(key) {
+            result.score += response[key];
+        }); 
+
+    // TODO: Verify case
+		
+        if(result.score <= 29) {
+            result.severity = 'Patient shows no signs of PTSD';
+            result.recommendation = 'N/A';
+        } else if(result.score <= 35) {
+            result.severity = 'Patient shows mild signs of PTSD';
+            result.recommendation = 'N/A';
+        } else if(result.score <= 44) {
+            result.severity = 'Patient shows moderate signs of PTSD';
+            result.recommendation = 'N/A';
+        } else {
+            result.severity = 'Patient shows severe signs of PTSD';
+            result.recommendation = 'N/A';
+        }
+	}
+
+  return {
+    result,
+    flags,
+  };
+}
